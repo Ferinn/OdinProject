ef40ef6e63b85c10f7be99d3471a6a0c90d06d22
Ferinn <ferin.pol@outlook.com>
2025-08-22 15:39:08 +0200

    Completed project Rock Paper Scissors

---
+++ Projects/Rock Paper Scissors/rockPaperScissors.js
@@ -0,0 +1,108 @@
+let playerScore = 0;
+let computerScore = 0;
+
+play();
+
+function play()
+{
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+
+    console.log("Player chose: " + playerChoice);
+    console.log("Computer chose: " + computerChoice);
+
+    if (playerChoice === null)
+    {
+        console.log("Please enter a valid choice!")
+        play();
+        return;
+    }
+    else
+    {
+        playerChoice = playerChoice.toUpperCase();
+        computerChoice = computerChoice.toUpperCase();
+
+        if (computerChoice === playerChoice)
+        {
+            console.log("It's a draw!");
+            return;
+        }
+
+        if (playerChoice === "ROCK")
+        {
+            if (computerChoice === "PAPER")
+            {
+                score(false);
+                return;
+            }
+            score(true);
+        }
+        else if (playerChoice === "PAPER")
+        {
+            if (computerChoice === "SCISSORS")
+            {
+                score(false);
+                return;
+            }
+            score(true);
+        }
+        else if (playerChoice === "SCISSORS")
+        {
+            if (computerChoice === "ROCK")
+            {
+                score(false);
+                return;
+            }
+            score(true);
+        }
+    }
+}
+
+function score(lastWonPlayer)
+{
+    if (lastWonPlayer === true)
+    {
+        playerScore++;
+        console.log("Player wins! :-)")
+    }
+    else
+    {
+        computerScore++;
+        console.log("Computer wins! :-(")
+    }
+
+    console.log("Player's score: " + playerScore);
+    console.log("Computer's score: " + computerScore);
+}
+
+function getComputerChoice()
+{
+    let pseudoRandom = Math.floor(Math.random() * 3);
+
+    switch (true)
+    {
+        case pseudoRandom === 0:
+            return "rock";
+        case pseudoRandom === 1:
+            return "paper";
+        case pseudoRandom === 2:
+            return "scissors";
+    }
+}
+
+function getPlayerChoice()
+{
+    let choice = prompt("Type 'rock' 'paper' or 'scissors' to enter your choice!", null);
+
+    switch (choice)
+    {
+        case "rock":
+            return "rock";
+        case "paper":
+            return "paper";
+        case "scissors":
+            return "scissors";
+    }
+    console.log("Invalid choice!")
+    return null;
+}
